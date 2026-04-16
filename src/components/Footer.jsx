81a1c1818e75c1814b331804ffe872ec0d8d8f0b
Noble <noblenwala.nn@gmail.com>
2026-04-16 11:53:55 +0100

    update copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="site-footer">
       <div className="site-footer__copy">
-        &copy; 2025 Noble Nwala - Designed &amp; Built with intent
+        &copy; 2026 Noble Nwala - Designed &amp; Built with intent
       </div>
     </footer>
   )
