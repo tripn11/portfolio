41e30766cea2c2b629e1be47659ca31d63d5fba8
Noble <noblenwala.nn@gmail.com>
2026-04-17 14:11:34 +0100

    modify year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="site-footer">
       <div className="site-footer__copy">
-        &copy; 2026 Noble Nwala - Designed &amp; Built with intent
+        &copy; 2026 Noble Nwala. All rights reserved.
       </div>
     </footer>
   )
